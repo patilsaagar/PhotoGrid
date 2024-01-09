84753bd7c25d80caaf65df7e260076470769d71d
sagar <patil.sagar27@gmail.com>
2024-01-09 22:00:50 +0530

    Moved ViewController to View

---
+++ PhotoGrid/View/ViewController.swift
@@ -0,0 +1,16 @@
+//
+//  ViewController.swift
+//  PhotoGrid
+//
+//  Created by Saggy on 09/01/24.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+}
+
